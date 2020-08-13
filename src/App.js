76ab0d2af7ef0b322148475b83fbeb2c651e7456
ajg7 <ajgebara@gmail.com>
2020-08-12 19:51:57 -0500

    created a photoObj which contains relevant data from nasa json

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [photoObj, setPhotoObj] = useState(null);
+
   useEffect(() => {
     axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-08-12")
     .then(response => {
-      console.log(response)
+      const { hdurl, title, copyright, date, explanation } = response.data;
+      const photoObj = {
+        hdurl: hdurl,
+        title: title,
+        copyright: copyright,
+        date: date,
+        explanation: explanation,
+      }
+      setPhotoObj(photoObj);
+    })
+    .catch(error => {
+      console.log(error);
     })
   }, [])
   
@@ -16,6 +29,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <Photo photoObj={photoObj}/>
     </div>
   );
 }
